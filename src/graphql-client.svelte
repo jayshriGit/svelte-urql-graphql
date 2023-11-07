cd3e26df2a031bbc8c9ea55fc457a151152fa865
jayshri.patil <jayshri.patil@globant.com>
2023-11-07 12:25:14 +0530

    package installation of urql

---
+++ src/graphql-client.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { Client, setContextClient, cacheExchange, fetchExchange } from '@urql/svelte';
+
+  const client = new Client({
+    url: 'http://localhost:3000/graphql',
+    exchanges: [cacheExchange, fetchExchange],
+  });
+
+  setContextClient(client);
+</script>
